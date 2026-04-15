b2f2123e3495b5142e9a78d34c993c8c20a23bdf
ts-dangelo <82030094+ts-dangelo@users.noreply.github.com>
2026-04-15 09:14:37 -0400

    Update 00-nf-mag-process-reads.sh

--- 00-nf-mag-process-reads.sh
+++ 00-nf-mag-process-reads.sh
@@ -16,4 +16,40 @@ nextflow run nf-core/mag \
     -c nextflow.mod.config
 
 
-## use find to pull the reads from the nf work dir, rename
+## pull the reads from the nf work dir and rename
+
+mkdir -p qc_reads
+
+find ./work \
+  -type f \
+  -name '*_run0_phix_removed.unmapped_*fastq.gz' \
+  -exec cp -t ./qc_reads/. {} +
+
+shopt -s nullglob
+
+for f in ./qc_reads/*.fastq.gz; do
+    base=$(basename "$f")
+
+    if [[ "$base" =~ (R1|_1)\.fastq\.gz$ ]]; then
+        read="_1"
+    elif [[ "$base" =~ (R2|_2)\.fastq\.gz$ ]]; then
+        read="_2"
+    else
+        continue
+    fi
+
+    stripped=${base/_R1/}
+    stripped=${stripped/_R2/}
+    stripped=${stripped/_1.fastq.gz/}
+    stripped=${stripped/_2.fastq.gz/}
+    stripped=${stripped%.fastq.gz}
+
+    stripped=${stripped/_run0_phix_removed.unmapped/_QC}
+
+    newname="qc_reads/${stripped}${read}.fastq.gz"
+
+    if [[ "$f" != "$newname" ]]; then
+        echo "  $base -> $(basename "$newname")"
+        mv -n "$f" "$newname"
+    fi
+done
